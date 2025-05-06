73b8c354b87b8deeafa227f759ce3440c0f1f08a
Md Hasibul Hasan <hhasibul677@gmail.com>
2025-05-06 14:51:51 +0600

    finished navigation section

--- src/components/Navigation/DesktopDropdown.tsx
+++ src/components/Navigation/DesktopDropdown.tsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom";
 import { NavigationLink } from "../../utils/contentTypes"
 
-interface DesktopDropdownProps{
+interface DesktopDropdownProps {
     links?: NavigationLink[];
 }
 
-export default function DesktopDropdown(props: DesktopDropdownProps) {
-  return (
-    <div>
-      dropdownLinks
-    </div>
-  )
+export default function DesktopDropdown({ links }: DesktopDropdownProps) {
+    return (
+        <ul className="invisible absolute top-full -left-8 z-1 flex translate-y-2 transform flex-col gap-y-7 rounded-tl-lg rounded-tr-2xl rounded-br-2xl rounded-bl-2xl bg-white pt-8 pr-16 pb-11 pl-8 opacity-0 drop-shadow-[2px_4px_30px_rgba(0,0,0,.10)] transition-all duration-200 ease-out group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+            {links?.map((link) =>
+                <li key={link.id} className="flex cursor-pointer items-center font-medium transition-all duration-200">
+                    <Link to={link.to}
+                        className="text-gray-600 text-nowrap hover:text-black">
+                        {link.text}
+                    </Link>
+                </li>)}
+        </ul>
+    )
 }
+
